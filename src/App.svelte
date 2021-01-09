dd4586895ca8e227e0d628c14a4740277698d490
Tam Minh Le <lemt93@gmail.com>
2021-01-10 00:30:24 +0700

    Init svelte project with postcss/tailwindcss setup

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <title>Tam Minh Le - Passionate Engineering</title>
+</svelte:head>
+
+<main class="text-red-400">
+  This is {fullName} CV
+</main>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<script>
+  let firstName = 'Tam'
+  let middleName = 'Minh'
+  let lastName = 'Le'
+
+  $: fullName = `${firstName} ${middleName} ${lastName}`
+</script>
+
